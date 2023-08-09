ed37f44dc51fa2c647f9d4ec1717d0e4485c785f
chandra9080962103 <141831119+chandra9080962103@users.noreply.github.com>
2023-08-09 18:46:45 +0530

    Create my_first_node.py

---
+++ my_first_node.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import rospy
+if __name__=='__main__':
+    rospy.init_node("test_node")
+
+    rospy.loginfo("test node has been started")
+    rate = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        rospy.loginfo("hello")
+        rate.sleep()
